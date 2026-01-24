cba853160216691cc17fa82a6a31feecce258687
VuongHuyHoang <hoangvuong.030705@gmail.com>
2026-01-24 18:25:36 +0700

    Cập nhật code
    sửa mới và thêm dòng

--- HelloWorld/src/til/edu/Xinchao.java
+++ HelloWorld/src/til/edu/Xinchao.java
@@ -13,7 +13,8 @@ public class Xinchao {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Tôi lần đầu học java, và đang thử nghiệm quản lý mã nguồn với GIT");
+		System.out.println("Tôi lần đầu học JAVA, và đang thử nghiệm quản lý mã nguồn với GIT");
+		System.out.println("Tôi thêm một dòng mới, và sửa lại dòng cũ");
 	}
 
 }
