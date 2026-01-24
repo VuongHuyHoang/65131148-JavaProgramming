cb4aa5da60cd960df0396744b022717419fc6be1
VuongHuyHoang <hoangvuong.030705@gmail.com>
2026-01-24 18:23:28 +0700

    Ví dụ đầu tiên
    Chỉ để demo

---
+++ HelloWorld/src/til/edu/Xinchao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package til.edu;
+
+/**
+ * 
+ */
+public class Xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tôi lần đầu học java, và đang thử nghiệm quản lý mã nguồn với GIT");
+	}
+
+}
